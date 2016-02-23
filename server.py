2192e6d8c7c7da1e86e1dbe86406d363c67c3c3c
97marcar <97marcar@bk-s2723x.local>
2016-02-23 09:40:05 +0100

    trying to make two clients to work
    currently only one works

--- server.py
+++ server.py
@@ -2,11 +2,12 @@ import socket
 import sys
 from _thread import *
 
-host = "195.196.201.102"
+host = ""
 port = 7777
 
 clients = []
-s.socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 try:
     s.bind((host,port))
@@ -16,8 +17,22 @@ except socket.error as e:
 s.listen(5)
 print("Waiting for connection...")
 
+
 def client(conn):
+    name = input("Enter Name: ")
     conn.send(str.encode("> "))
 
     while True:
         data = conn.recv(2048)
+        reply = name +": "+ data.decode("utf-8")
+        if not data:
+            break
+        conn.sendall(str.encode(reply))
+    conn.close()
+
+while True:
+    conn, addr = s.accept()
+    print("connected to: "+addr[0]+":"+str(addr[1]))
+    print("connected to: "+addr2[0]+":"+str(addr2[1]))
+
+    start_new_thread(client(conn,), client(conn2,))
