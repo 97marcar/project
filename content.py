059bc8682a2e44ee4bae838a9806a37cff26c4d7
97marcar <97marcar@bk-s2723x.local>
2016-03-15 10:05:26 +0100

    Added a list of commands and ability to pick up note

--- content.py
+++ content.py
@@ -1,4 +1,4 @@
-from room import *
+from classes import *
 
 class Content:
     def __init__(self, position):
@@ -8,13 +8,45 @@ class Content:
         self.rooms = []
         self.zero()
         self.one()
+        self.note = Note()
+        self.list_of_commands()
 
+    def list_of_commands(self):
+        self.compass = [["n", "go n", "north", "go north"],
+        ["s", "go s", "south", "go south"],
+        ["w", "go w", "west", "go west"],
+        ["e", "go e", "east", "go east"],
+        ["nw", "go nw", "northwest", "go northwest"],
+        ["ne", "go ne", "northeast", "go northeast"],
+        ["sw", "go sw", "southwest", "go southwest"],
+        ["se", "go se", "southeast", "go southeast"]]
 
+        self.takeNote = ["take note", "pick up note"]
 
     def handle_command(self, command):
+        #Main Menu
         if self.pos == 0 and command == "begin":
             self.pos = 1
             return (self.get_description(),"clear")
+
+        #General
+        elif self.pos == self.note.position and command in self.takeNote \
+        and self.note.status == "dropped":
+            self.note.status = "picked up"
+            return ("You picked up a note.", "note")
+
+        elif self.note.status == "picked up" and command == "read note":
+            return (self.note.description, "")
+
+        elif command in self.compass[0]:
+            return("123","")
+        elif command in self.compass[1]or[2]or[3]or[4]or[5]or[6]:
+            return("1234","")
+
+        else:
+            return ("I beg your pardon?", "")
+
+
 
 
     def zero(self):
@@ -33,7 +65,8 @@ class Content:
         name = "Wooden House"
         description = ("You are standing in the front of a wooden house.\n"
         "It looks like there is forest in all directions, all though it looks like "
-        "there is a path to the west.")
+        "there is a path to the west.\n"
+        "It's a note laying on the ground.")
         room_1 = Room(position,name,description)
         self.rooms.append(room_1)
 
